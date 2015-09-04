7523ac9eb2641b44a04ee248c39ed7595d279dc2
Mark Roan Fado <mfado@zendesk.com>
2015-09-04 22:14:39 +0800

    indentation

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
 (function() {
 
-  return {
-    events: {
-      'app.activated':'loadIDs'
-    },
+        return {
+            events: {
+                'app.activated': 'loadIDs'
+            },
 
-    requests: {
-        getExtID: function(endPoint,itemID) {
-            return {
-                url: '/api/v2/'+ endPoint +'/'+ itemID +'.json',
-                type:'GET'
-            };
-        }
-    },    
+            requests: {
+                getExtID: function(endPoint, itemID) {
+                    return {
+                        url: '/api/v2/' + endPoint + '/' + itemID + '.json',
+                        type: 'GET'
+                    };
+                }
+            },
 
-    loadIDs: function() {
-		var ticket = this.ticket();
-		var ticketID = ticket.id();
-		var orgID = ticket.organization().id();
+            loadIDs: function() {
+                var ticket = this.ticket();
+                var ticketID = ticket.id();
+                var orgID = ticket.organization().id();
 
-		if (orgID) {
-	   		this.ajax('getExtID', 'organizations', orgID).done(function(data) {
-	            console.log(data.organization.external_id;
-	         }); 		
-		}
-					
-   		this.ajax('getExtID', 'tickets', ticketID).done(function(data) {
-            console.log(data.ticket.external_id);
-         }); 
+                if (orgID) {
+                    this.ajax('getExtID', 'organizations', orgID).done(function(data) {
+                            console.log(data.organization.external_id;
+                            });
+                    }
 
-		var requesterEID = ticket.requester().externalId();
+                    this.ajax('getExtID', 'tickets', ticketID).done(function(data) {
+                        console.log(data.ticket.external_id);
+                    });
 
-  		
+                    var requesterEID = ticket.requester().externalId();
 
-   		/*
+
+
+                    /*
 		var promiseOrg = this.ajax('getExtID', 'tickets', ticketID)            
 
 		this.when(promiseTicket).then(
@@ -43,8 +43,8 @@
             function() {
               console.log('Failure');
             }.bind(this));     
-        */    
-    }
-  };
+        */
+                }
+            };
 
-}());
+        }());
