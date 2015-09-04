9abc78ba9a444b0b312f1419ef20746bf1695faf
Mark Roan Fado <mfado@zendesk.com>
2015-09-04 22:13:31 +0800

    ext ID logic

--- app.js
+++ app.js
@@ -2,10 +2,48 @@
 
   return {
     events: {
-      'app.activated':'doSomething'
+      'app.activated':'loadIDs'
     },
 
-    doSomething: function() {
+    requests: {
+        getExtID: function(endPoint,itemID) {
+            return {
+                url: '/api/v2/'+ endPoint +'/'+ itemID +'.json',
+                type:'GET'
+            };
+        }
+    },    
+
+    loadIDs: function() {
+		var ticket = this.ticket();
+		var ticketID = ticket.id();
+		var orgID = ticket.organization().id();
+
+		if (orgID) {
+	   		this.ajax('getExtID', 'organizations', orgID).done(function(data) {
+	            console.log(data.organization.external_id;
+	         }); 		
+		}
+					
+   		this.ajax('getExtID', 'tickets', ticketID).done(function(data) {
+            console.log(data.ticket.external_id);
+         }); 
+
+		var requesterEID = ticket.requester().externalId();
+
+  		
+
+   		/*
+		var promiseOrg = this.ajax('getExtID', 'tickets', ticketID)            
+
+		this.when(promiseTicket).then(
+            function(ticketData) {
+            	console.log(ticketData.ticket.brand_id);
+            }.bind(this),
+            function() {
+              console.log('Failure');
+            }.bind(this));     
+        */    
     }
   };
 
